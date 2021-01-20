c8912c2d699afb208b052268e66b2dbabe56f49b
Yuri Alves <yuripiresalves@gmail.com>
2021-01-20 13:35:16 -0300

    finished

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import ButtonModal from './ButtonModal';
-import Modal from './Modal';
+import Produto from './Produto';
 
 const App = () => {
-  const [modal, setModal] = React.useState(false);
+  const [dados, setDados] = React.useState(null);
+  const [carregando, setCarregando] = React.useState(null);
+
+  async function handleClick(event) {
+    setCarregando(true);
+    const response = await fetch(
+      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`
+    );
+    const json = await response.json();
+    setDados(json);
+    setCarregando(false);
+  }
 
   return (
     <div>
-      <Modal modal={modal} setModal={setModal} />
-      <ButtonModal setModal={setModal} />
+      <button style={{ margin: '.5rem' }} onClick={handleClick}>
+        notebook
+      </button>
+      <button style={{ margin: '.5rem' }} onClick={handleClick}>
+        smartphone
+      </button>
+      <button style={{ margin: '.5rem' }} onClick={handleClick}>
+        tablet
+      </button>
+      {carregando && <p>Carregando...</p>}
+      {!carregando && dados && <Produto dados={dados} />}
     </div>
   );
 };
