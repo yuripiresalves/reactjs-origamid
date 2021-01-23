ac3be4a807a1973e4c473ab1d3aa49899c7d3e10
Yuri Alves <yuripiresalves@gmail.com>
2021-01-23 20:08:41 -0300

    first commit

---
+++ hooks/useContext/src/GlobalContext.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export const GlobalContext = React.createContext();
+
+export const GlobalStorage = ({ children }) => {
+  const [contar, setContar] = React.useState(0);
+
+  function AdicionarUm() {
+    setContar((contar) => contar + 1);
+  }
+
+  function AdicionarDois() {
+    setContar((contar) => contar + 2);
+  }
+
+  return (
+    <GlobalContext.Provider value={{ contar, AdicionarUm, AdicionarDois }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
