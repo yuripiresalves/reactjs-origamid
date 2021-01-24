b1a79915707dd873db7c472ae144d36a423cc0a6
Yuri Alves <yuripiresalves@gmail.com>
2021-01-23 22:23:32 -0300

    finished

--- hooks/CustomHooks/src/App.js
+++ hooks/CustomHooks/src/App.js
@@ -1,22 +1,45 @@
 import React from 'react';
 import useLocalStorage from './useLocalStorage';
+import useFetch from './useFetch';
 
 const App = () => {
   const [produto, setProduto] = useLocalStorage('produto', '');
+  const { request, data, loading, error } = useFetch();
+
+  React.useEffect(() => {
+    async function fetchData() {
+      const { response, json } = await request(
+        'https://ranekapi.origamid.dev/json/api/produto/'
+      );
+      console.log(response, json);
+    }
+    fetchData();
+  }, [request]);
+  console.log(data);
 
   function handleClick({ target }) {
     setProduto(target.innerText);
   }
 
-  return (
-    <div>
-      <p>Produto: {produto}</p>
-      <button style={{ marginRight: '1rem' }} onClick={handleClick}>
-        notebook
-      </button>
-      <button onClick={handleClick}>smartphone</button>
-    </div>
-  );
+  if (error) return <p>{error}</p>;
+  if (loading) return <p>Carregando...</p>;
+  if (data)
+    return (
+      <div>
+        <p>Produto preferido: {produto}</p>
+        <button style={{ marginRight: '1rem' }} onClick={handleClick}>
+          notebook
+        </button>
+        <button onClick={handleClick}>smartphone</button>
+
+        {data.map((produto) => (
+          <div key={produto.id}>
+            <h1>{produto.nome}</h1>
+          </div>
+        ))}
+      </div>
+    );
+  else return null;
 };
 
 export default App;
