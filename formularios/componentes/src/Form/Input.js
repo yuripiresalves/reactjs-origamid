69a1b1c9acfe6be19b10b5dbb07d8f36f1aa5f14
Yuri Alves <yuripiresalves@gmail.com>
2021-02-05 09:37:35 -0300

    add props

--- formularios/componentes/src/Form/Input.js
+++ formularios/componentes/src/Form/Input.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Input = ({ id, label, value, setValue }) => {
+const Input = ({ id, label, value, setValue, ...props }) => {
   return (
     <>
       <label htmlFor={id}>{label}</label>
@@ -10,6 +10,7 @@ const Input = ({ id, label, value, setValue }) => {
         name={id}
         value={value}
         onChange={({ target }) => setValue(target.value)}
+        {...props}
       />
     </>
   );
