e411abf32abb362e3687aaa44716f3cdc761d964
Yuri Alves <yuripiresalves@gmail.com>
2021-02-05 09:37:57 -0300

    add Select

--- formularios/componentes/src/App.js
+++ formularios/componentes/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import Input from './Form/Input';
+import Select from './Form/Select';
 
 const App = () => {
   const [nome, setNome] = React.useState('');
   const [email, setEmail] = React.useState('');
+  const [produto, setProduto] = React.useState('');
 
   return (
     <form>
-      <Input id="nome" label="Nome" value={nome} setValue={setNome} />
+      <Select
+        options={['Smartphone', 'Notebook', 'Tablet']}
+        value={produto}
+        setValue={setProduto}
+      />
+      <Input id="nome" label="Nome" value={nome} setValue={setNome} required />
       <Input id="email" label="E-mail" value={email} setValue={setEmail} />
+      <button>Enviar</button>
     </form>
   );
 };
