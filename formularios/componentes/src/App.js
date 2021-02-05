def543fb84cf786b2402ee41f393ca4edf973b05
Yuri Alves <yuripiresalves@gmail.com>
2021-02-05 10:28:36 -0300

    add Checkbox

--- formularios/componentes/src/App.js
+++ formularios/componentes/src/App.js
@@ -2,16 +2,31 @@ import React from 'react';
 import Input from './Form/Input';
 import Select from './Form/Select';
 import Radio from './Form/Radio';
+import Checkbox from './Form/Checkbox';
 
 const App = () => {
   const [nome, setNome] = React.useState('');
   const [email, setEmail] = React.useState('');
   const [produto, setProduto] = React.useState('');
-  const [cor, setCor] = React.useState('');
+  const [cor, setCor] = React.useState('Vermelho');
   const [fruta, setFruta] = React.useState('');
+  const [linguagem, setLinguagem] = React.useState([]);
+  const [termos, setTermos] = React.useState([]);
 
   return (
     <form>
+      <h2>Termos</h2>
+      <Checkbox
+        options={['Li e aceito os termos.']}
+        value={termos}
+        setValue={setTermos}
+      />
+      <h2>Checkbox</h2>
+      <Checkbox
+        options={['JavaScript', 'C', 'Python']}
+        value={linguagem}
+        setValue={setLinguagem}
+      />
       <h2>Cores</h2>
       <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor} />
       <h2>Frutas</h2>
