bc98af638fdb39ee17cd523b29e254e878f614c0
Yuri Alves <yuripiresalves@gmail.com>
2021-02-09 11:47:49 -0300

    first commit

---
+++ router/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Header';
+import Home from './Home';
+import Sobre from './Sobre';
+import NaoEncontrada from './NaoEncontrada';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="sobre" element={<Sobre />} />
+        <Route path="*" element={<NaoEncontrada />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
