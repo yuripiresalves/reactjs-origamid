89972605c6dbff53948eb68ac7a64c7351747360
Yuri Alves <yuripiresalves@gmail.com>
2021-02-09 12:47:50 -0300

    add Head

--- router/src/Home.js
+++ router/src/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Head from './Head';
 
 const Home = () => {
   return (
     <div>
+      <Head title="Home" description="Essa é a descrição da Home" />
       <h1>Home</h1>
       <p>Essa é a home</p>
       <Link to="produto/notebook">Notebook</Link>{' '}
