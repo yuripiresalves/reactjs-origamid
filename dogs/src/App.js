fabb38d0a7716c6c7de2bb8dfb34011879384840
Yuri Alves <yuripiresalves@gmail.com>
2021-02-18 12:58:12 -0300

    add ProtectedRoute

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -6,6 +6,8 @@ import Footer from './Components/Footer';
 import Home from './Components/Home';
 import Login from './Components/Login';
 import { UserStorage } from './UserContext';
+import User from './Components/User';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
 
 const App = () => {
   return (
@@ -15,7 +17,8 @@ const App = () => {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <ProtectedRoute path="conta/*" element={<User />} />
           </Routes>
           <Footer />
         </UserStorage>
