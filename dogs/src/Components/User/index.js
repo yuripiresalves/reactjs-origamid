973258ae7f8a8de34c1376dd392866d9b182503f
Yuri Alves <yuripiresalves@gmail.com>
2021-02-19 12:02:00 -0300

    add UserHeader

--- dogs/src/Components/User/index.js
+++ dogs/src/Components/User/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import UserHeader from './UserHeader';
+import UserPhotoPost from './UserPhotoPost';
+import UserStats from './UserStats';
+import Feed from '../Feed';
 
 const User = () => {
-  return <div>Usuario</div>;
+  return (
+    <section className="container">
+      <UserHeader />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="postar" element={<UserPhotoPost />} />
+        <Route path="estatisticas" element={<UserStats />} />
+      </Routes>
+    </section>
+  );
 };
 
 export default User;
