8d0ea7a6c2fe5fef56de603ac40dbfe113593031
Yuri Alves <yuripiresalves@gmail.com>
2021-03-03 11:04:12 -0300

    add infinite scroll

--- dogs/src/Components/Feed/FeedPhotos.js
+++ dogs/src/Components/Feed/FeedPhotos.js
@@ -6,18 +6,18 @@ import Loading from '../Helper/Loading';
 import { PHOTOS_GET } from '../../api';
 import styles from './FeedPhotos.module.css';
 
-const FeedPhotos = ({ setModalPhoto }) => {
+const FeedPhotos = ({ user, page, setModalPhoto, setInfinite }) => {
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
     async function fetchPhotos() {
-      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+      const total = 6;
+      const { url, options } = PHOTOS_GET({ page, total, user });
       const { response, json } = await request(url, options);
-      console.log(json);
+      if (response && response.ok && json.length < total) setInfinite(false);
     }
     fetchPhotos();
-  }, [request]);
-  console.log('data', data);
+  }, [request, user, page, setInfinite]);
 
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;
