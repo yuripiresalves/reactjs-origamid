0aa0587f6df0b4b38b3ea1a2ecf045d15676efe4
Yuri Alves <yuripiresalves@gmail.com>
2021-03-03 11:03:54 -0300

    add user data

--- dogs/src/Components/User/index.js
+++ dogs/src/Components/User/index.js
@@ -4,13 +4,16 @@ import UserHeader from './UserHeader';
 import UserPhotoPost from './UserPhotoPost';
 import UserStats from './UserStats';
 import Feed from '../Feed';
+import { UserContext } from '../../UserContext';
 
 const User = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <section className="container">
       <UserHeader />
       <Routes>
-        <Route path="/" element={<Feed />} />
+        <Route path="/" element={<Feed user={data.id} />} />
         <Route path="postar" element={<UserPhotoPost />} />
         <Route path="estatisticas" element={<UserStats />} />
       </Routes>
