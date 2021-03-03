c173cc49731edc4393a38c4d04c0b5f49ed933a5
Yuri Alves <yuripiresalves@gmail.com>
2021-03-03 11:53:11 -0300

    add NotFound page

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -10,6 +10,7 @@ import User from './Components/User';
 import ProtectedRoute from './Components/Helper/ProtectedRoute';
 import Photo from './Components/Photo';
 import UserProfile from './Components/User/UserProfile';
+import NotFound from './Components/NotFound';
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
             <ProtectedRoute path="conta/*" element={<User />} />
             <Route path="foto/:id" element={<Photo />} />
             <Route path="perfil/:user" element={<UserProfile />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
           <Footer />
         </UserStorage>
--- dogs/src/Components/User/index.js
+++ dogs/src/Components/User/index.js
@@ -5,6 +5,7 @@ import UserPhotoPost from './UserPhotoPost';
 import UserStats from './UserStats';
 import Feed from '../Feed';
 import { UserContext } from '../../UserContext';
+import NotFound from '../NotFound';
 
 const User = () => {
   const { data } = React.useContext(UserContext);
@@ -16,6 +17,7 @@ const User = () => {
         <Route path="/" element={<Feed user={data.id} />} />
         <Route path="postar" element={<UserPhotoPost />} />
         <Route path="estatisticas" element={<UserStats />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </section>
   );
