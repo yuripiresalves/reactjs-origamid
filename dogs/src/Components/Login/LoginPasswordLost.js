76d677838715a7b2228ee7202966bf0fe7abdd5e
Yuri Alves <yuripiresalves@gmail.com>
2021-03-04 10:44:39 -0300

    add className animeLeft

--- dogs/src/Components/Login/LoginPasswordLost.js
+++ dogs/src/Components/Login/LoginPasswordLost.js
@@ -23,7 +23,7 @@ const LoginPasswordLost = () => {
   }
 
   return (
-    <section>
+    <section className="animeLeft">
       <Head
         title="Perdeu a senha"
         description="Página para recuperação de senha"
